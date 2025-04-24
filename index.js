d124f708b26daa964b1766bfe04fd2e110b5c9ce
HEITOR HILLESHEIM DOS SANTOS <heitor_h_santos@estudante.sesisenai.org.br>
2025-04-24 07:39:21 -0300

    instanciate bugfix2

--- index.js
+++ index.js
@@ -6,22 +6,3 @@ class personagem{
     }
 }
 const personagem1 = new personagem(10,10,10)
-
-class mago extends personagem{
-    constructor(vida,forca,mana,magia){
-        super(vida,forca,mana)
-        this.magia = magia
-    }
-}
-class guerreiro extends personagem{
-    constructor(vida,forca,mana,armadura){
-        super(vida,forca,mana)
-        this.armadura = armadura
-    }
-}
-class arqueiro extends personagem{
-    constructor(vida,forca,mana,flechas){
-        super(vida,forca,mana)
-        this.flechas = flechas
-    }
-}
