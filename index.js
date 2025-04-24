246873a22919d87a9ffe896e24f2207f8875062a
HEITOR HILLESHEIM DOS SANTOS <heitor_h_santos@estudante.sesisenai.org.br>
2025-04-24 08:28:03 -0300

    classe main criada para gerenciamento do jogo

--- index.js
+++ index.js
@@ -1,77 +1,120 @@
-class personagem{
+class personagem {
     #vida
     #forca
     #mana
-    set vida(value){
-        if (value>20){
+    set vida(value) {
+        if (value > 20) {
             throw new Error("vida maior que 20")
         }
         this.#vida = value
     }
-    get vida(){
+    get vida() {
         return this.#vida
     }
-    set forca(value){
-        if (value>15){
+    set forca(value) {
+        if (value > 15) {
             throw new Error("forca maior que 15")
         }
         this.#forca = value
     }
-    get forca(){
+    get forca() {
         return this.#forca
     }
-    set mana(value){
-        if (value>40){
+    set mana(value) {
+        if (value > 40) {
             throw new Error("mana maior que 40")
         }
         this.#mana = value
     }
-    get mana(){
+    get mana() {
         return this.#mana
     }
-    constructor(vida,forca,mana){
+    constructor(vida, forca, mana) {
         this.#vida = vida;
         this.#forca = forca;
         this.#mana = mana;
     }
-    atacar(personagem){
-        personagem.vida-=this.#forca
+    atacar(personagem) {
+        personagem.vida -= this.#forca
+        console.log("")
+        console.log("Atacante: ")
+        this.exibir()
+        console.log("Vítima: ")
+        personagem.exibir()
+        console.log("")
+    }
+    exibir() {
+        console.log("O personagem está com: ", this.#vida, " vida; ", this.#mana, " mana; ", this.#forca, " forca")
     }
 
 }
-const personagem1 = new personagem(10,10,10)
-class mago extends personagem{
-    constructor(vida,forca,mana,magia){
-        super(vida,forca,mana)
+const personagem1 = new personagem(10, 10, 10)
+class mago extends personagem {
+    constructor(vida, forca, mana, magia) {
+        super(vida, forca, mana)
         this.magia = magia
     }
-    usarMagia(){
+    usarMagia() {
         console.log("magia")
     }
+    exibir() {
+        console.log("O mago está com: ", this.vida, " vida; ", this.mana, " mana; ", this.forca, " forca; ", this.magia, " magia")
+    }
 
 }
-class guerreiro extends personagem{
-    constructor(vida,forca,mana,armadura){
-        super(vida,forca,mana)
+class guerreiro extends personagem {
+    constructor(vida, forca, mana, armadura) {
+        super(vida, forca, mana)
         this.armadura = armadura
     }
-    defender(){
+    defender() {
         console.log("defendido")
     }
-
+    exibir() {
+        console.log("O guerreiro está com: ", this.vida, " vida; ", this.mana, " mana; ", this.forca, " forca; ", this.armadura, " armadura")
+    }
 }
-class arqueiro extends personagem{
-    constructor(vida,forca,mana,flechas){
-        super(vida,forca,mana)
+class arqueiro extends personagem {
+    constructor(vida, forca, mana, flechas) {
+        super(vida, forca, mana)
         this.flechas = flechas
     }
-    atirar(){
+    atirar() {
         console.log("*flecha*")
         this.flechas--
     }
-    
+    exibir() {
+        console.log("O arqueiro está com: ", this.vida, " vida; ", this.mana, " mana; ", this.forca, " forca; ", this.flechas, " flechas")
+    }
 
 }
-const arqueiro1 = new arqueiro(10,1,10,10)
-const guerreiro1 = new guerreiro(10,1,10,10)
-const mago1 = new mago(10,1,10,10)
+
+class main {
+    #personagens
+    get personagens() {
+        return this.#personagens
+    }
+    set personagens(value) {
+        this.#personagens = value
+    }
+    constructor(personagens) {
+        this.#personagens = personagens
+    }
+}
+
+const main1 = new main([
+    new guerreiro(10, 2, 0, 20),
+    new arqueiro(10, 1, 0, 30),
+    new mago(10, 3, 20, 23)
+])
+
+function batalhaGeral(main) { //método criado para testar a função de batalha, para o critério 2 da atividade
+    for (let i = 0; i < main.personagens.length; i++) {
+        for (let a = 0; a < main.personagens.length; a++) {
+            if (a != i) {
+                main.personagens[i].atacar(main.personagens[a])
+            }
+        }
+    }
+}
+batalhaGeral(main1)
